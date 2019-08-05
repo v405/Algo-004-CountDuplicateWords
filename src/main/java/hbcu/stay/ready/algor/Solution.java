610a32a9a7a61587a22a26512453366db029f731
v405 <valerieandy72@gmail.com>
2019-08-05 09:42:33 -0400

    Finished

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,31 @@
 package hbcu.stay.ready.algor;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
-    public String countUniqueWords(String input){
-        return null;
+    public String countUniqueWords(String input) {
+        String[] strArray = input.split(" ");
+        Map<String, Integer> mapA = new HashMap();
+        for (String str : strArray) {
+            if (mapA.containsKey(str)) {
+                int counter = mapA.get(str);
+                mapA.put(str, ++counter);
+            } else {
+                mapA.put(str, 1);
+            }
+
+        }
+
+        for (String str : mapA.keySet()){
+            int duplicateC = mapA.get(str);
+        }
+
+        return "The 3 unique words are:\n" +
+                "(Seen " + mapA.get(0)+" \n" +
+                " "+"(Seen " + mapA.get(1)+" \n" +
+                " "+"(Seen " + mapA.get(2)+" \n";
+
+
     }
 }
