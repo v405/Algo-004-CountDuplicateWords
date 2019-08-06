e725c4598a94200cd53f4825da77ba9e0d360046
v405 <valerieandy72@gmail.com>
2019-08-05 22:09:51 -0400

    Finished

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,5 +1,6 @@
 package hbcu.stay.ready.algor;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Solution {
@@ -14,18 +15,35 @@ public class Solution {
             } else {
                 mapA.put(str, 1);
             }
-
         }
 
-        for (String str : mapA.keySet()){
-            int duplicateC = mapA.get(str);
+        Integer numberOfUniqueWords = numOfOccurences(mapA);
+
+        String emptyString = "";
+        emptyString += "The " + numberOfUniqueWords + " unique words are:\n";
+
+        Iterator<Map.Entry<String, Integer>> itr = mapA.entrySet().iterator();
+
+        while(itr.hasNext())
+        {
+            Map.Entry<String, Integer> mapinput = itr.next();
+            if (!itr.hasNext()){
+                emptyString += mapinput.getKey() + " (seen " + mapinput.getValue() + ")";
+            }
+            else{
+                emptyString += mapinput.getKey() + " (seen " + mapinput.getValue() + ")";
+                emptyString += "\n";
+            }
         }
 
-        return "The 3 unique words are:\n" +
-                "(Seen " + mapA.get(0)+" \n" +
-                " "+"(Seen " + mapA.get(1)+" \n" +
-                " "+"(Seen " + mapA.get(2)+" \n";
+        return emptyString;
+    }
 
-
+    private Integer numOfOccurences(Map<String, Integer> map){
+        Integer count = 0;
+        for(Map.Entry<String, Integer> input: map.entrySet()){
+            count ++;
+        }
+        return count;
     }
 }
